d199174c8a4fdc08c4976b2ddbf505a00850fb38
woraseth <woraseth@users.noreply.github.com>
2015-09-11 11:28:30 +0700

    Create ImageTextAdapter.java

---
+++ ImageTextAdapter.java
@@ -0,0 +1,33 @@
+    // tested with Spinner
+    
+    public class ImageTextAdapter extends ArrayAdapter<String> {
+
+        int[] images;
+
+        public ImageTextAdapter(Context ctx, String[] texts, int[] images) {
+            super(ctx, R.layout.row, texts);
+            this.images = images;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return getCustomView(position, convertView, parent);
+        }
+
+        public View getCustomView(int position, View convertView, ViewGroup parent) {
+            View row = getLayoutInflater().inflate(R.layout.row, parent, false);
+
+            TextView textView = (TextView) row.findViewById(R.id.textView);
+            textView.setText(getItem(position));
+
+            ImageView imageView = (ImageView) row.findViewById(R.id.imageView);
+            imageView.setImageResource(images[position]);
+
+            return row;
+        }
+    }
